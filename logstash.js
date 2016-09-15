3b685ca70e818772aea2dd11bb02f1c5cd670910
Emer Rutherford <emer.rutherford@nearform.com>
2016-09-15 16:25:04 +0100

    Added seneca-log-filter.

--- logstash.js
+++ logstash.js
@@ -1,15 +1,20 @@
 'use strict'
 
 var Logstash = require('logstash-client')
+var LogFilter = require('seneca-log-filter')
 
 function Logger () {}
 
 Logger.preload = function () {
   var options = this.options()
 
+  var logFilter = LogFilter(options['log'])
   var client = new Logstash(options['logstash-logger'])
   function adapter (context, payload) {
-    client.send(payload)
+    let filtered = logFilter(payload)
+    if(filtered) {
+      client.send(payload)
+    }
   }
 
   return {
